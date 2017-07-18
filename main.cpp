e95714a82f2387be027f6a280c7c32c4e60d9e54
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-18 21:18:10 +0200

    add the problem into the main file

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+
+class MyClass {
+
+public:
+
+    void action() const & noexcept {
+
+        constexpr unsigned short ITERATIONS {10};
+        for (
+            auto i = 0;
+            i < ITERATIONS;
+            i += 1
+        ) {
+            constexpr unsigned short TIME {1000};
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(TIME)
+            );
+
+            value += 1;
+
+            std::cout << value << std::endl;
+        }
+    }
+
+    void process() const & noexcept {
+
+        std::thread firstThread(
+            &MyClass::action,
+            this
+        );
+
+        std::thread secondThread(
+            &MyClass::action,
+            this
+        );
+
+        firstThread.join();
+        secondThread.join();
+    }
+
+private:
+
+    mutable int value {0};
+};
+
+int main() {
+
+    MyClass object;
+    object.process();
+
+    return EXIT_SUCCESS;
+}
